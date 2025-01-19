597a77b4ee5c36e7aa82279c7bf9fad445df1f6e
jathevan <jathevan15@gmail.com>
2025-01-20 01:23:05 +1000

    Initial commit with Gradle setup and Java 22

---
+++ src/main/java/com/java/Main.java
@@ -0,0 +1,7 @@
+package com.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions with Gradle and Java 22!");
+    }
+}
---
+++ src/test/java/com/java/MainTest.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
